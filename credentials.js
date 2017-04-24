7c96f1403849bdf24507d5fa24a795b336257933
yaphtes <yaphtes.ks@gmail.com>
2017-04-24 22:29:13 +0400

    restart project

--- credentials.js
+++ credentials.js
@@ -1,5 +1,5 @@
 module.exports = {
     mongo: 'mongodb://localhost:27017/keeper',
     jwtSecret: 'secret',
-    tokenExpires: Math.floor(Date.now() / 1000) + 60 * 60 * 24 * 30 // месяц
+    tokenExpires: Math.floor(Date.now() / 1000) + 40 * 1 * 1 * 1 // четверть минуты
 };
--- routes.js
+++ routes.js
@@ -1,12 +1,6 @@
-const users = require('./handlers/users');
+const users = require('./controllers/users');
 
 module.exports = function (app, db) {
 
-    // check token route
-    app.use(users.checkToken(app, db));
-
-    // users routes
-    app.get('/login', users.login(app, db));
-    app.get('/user', users.getUser(app, db));
-    app.post('/user', users.postUser(app, db));
+    // app.use(users.checkToken(app, db));
 };
